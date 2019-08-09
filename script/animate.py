ee74ce4a7b82420d8f91eac124abe10f29fe720c
David Gonon <david.gonon@epfl.ch>
2019-08-09 19:29:24 +0200

    Added body shapes to urdf

--- script/animate.py
+++ script/animate.py
@@ -15,56 +15,152 @@ import matplotlib.pyplot as plt
 # 	applyMMMRotationToURDFJoint(urdf_body_id, 3,
 # 		joint_positions[14], joint_positions[15], joint_positions[16])
 
+def generateQuaternionFromMMMRxRyRz(rx, ry, rz):
+	q_intermediate = p.getQuaternionFromEuler([math.pi/2, 0, math.pi/2])
+	t, q_intermediate_inv = p.invertTransform([0,0,0], q_intermediate)
+	q_tmp = p.getQuaternionFromEuler([ry, rz, rx])
+	t, q_tmp = p.multiplyTransforms([0,0,0], q_intermediate, [0,0,0], q_tmp)
+	t, q = p.multiplyTransforms([0,0,0], q_tmp, [0,0,0], q_intermediate_inv)
+	return q
+
+# def generateQuaternionFromMMMGlobalRxRyRz(rx, ry, rz):
+# 	q_intermediate = p.getQuaternionFromEuler([0, -math.pi/2, math.pi])
+# 	t, q_intermediate_inv = p.invertTransform([0,0,0], q_intermediate)
+# 	q_tmp = p.getQuaternionFromEuler([rz, -ry, rx])
+# 	t, q_tmp = p.multiplyTransforms([0,0,0], q_intermediate, [0,0,0], q_tmp)
+# 	t, q = p.multiplyTransforms([0,0,0], q_tmp, [0,0,0], q_intermediate_inv)
+# 	return q
+
 def applyMMMRotationToURDFJoint(urdf_body_id, joint_index, rx, ry, rz, inverse=False):
-	quat_0 = p.getQuaternionFromEuler([rx,rz,-ry])
-
-	quat_tf = p.getQuaternionFromEuler([math.pi/2,0,0])
-	translation, quat_tf_inv = p.invertTransform([0,0,0], quat_tf)
-
-	quat_tf_urdf = p.getQuaternionFromEuler([-math.pi/2,0,-math.pi])
+	q = generateQuaternionFromMMMRxRyRz(rx, ry, rz)
+	quat_tf_urdf = p.getQuaternionFromEuler([-math.pi/2, math.pi, 0])
 	translation, quat_tf_urdf_inv = p.invertTransform([0,0,0], quat_tf_urdf)
-
-	translation, quat_tmp = p.multiplyTransforms([0,0,0], quat_tf_urdf,
-		[0,0,0], quat_tf)
-
-	translation, quat_tmp = p.multiplyTransforms([0,0,0], quat_tmp,
-		[0,0,0], quat_0)
-
-	translation, quat_tmp = p.multiplyTransforms([0,0,0], quat_tmp,
-		[0,0,0], quat_tf_inv)
-
-	translation, quat_final = p.multiplyTransforms([0,0,0], quat_tmp,
-		[0,0,0], quat_tf_urdf_inv)
+	t, q = p.multiplyTransforms([0,0,0], quat_tf_urdf, [0,0,0], q)
+	t, q = p.multiplyTransforms([0,0,0], q, [0,0,0], quat_tf_urdf_inv)
 
 	if inverse:
-		translation, quat_final = p.invertTransform([0,0,0], quat_final)
+		t, q = p.invertTransform([0,0,0], q)
 
-	p.resetJointStateMultiDof(urdf_body_id, joint_index, quat_final)
+	p.resetJointStateMultiDof(urdf_body_id, joint_index, q)
+
+	# quat_0 = p.getQuaternionFromEuler([rx,rz,-ry])
+
+	# quat_tf = p.getQuaternionFromEuler([math.pi/2,0,0])
+	# translation, quat_tf_inv = p.invertTransform([0,0,0], quat_tf)
+
+	# quat_tf_urdf = p.getQuaternionFromEuler([-math.pi/2,0,-math.pi])
+	# translation, quat_tf_urdf_inv = p.invertTransform([0,0,0], quat_tf_urdf)
+
+	# translation, quat_tmp = p.multiplyTransforms([0,0,0], quat_tf_urdf,
+	# 	[0,0,0], quat_tf)
+
+	# translation, quat_tmp = p.multiplyTransforms([0,0,0], quat_tmp,
+	# 	[0,0,0], quat_0)
+
+	# translation, quat_tmp = p.multiplyTransforms([0,0,0], quat_tmp,
+	# 	[0,0,0], quat_tf_inv)
+
+	# translation, quat_final = p.multiplyTransforms([0,0,0], quat_tmp,
+	# 	[0,0,0], quat_tf_urdf_inv)
+
+	# if inverse:
+	# 	translation, quat_final = p.invertTransform([0,0,0], quat_final)
+
+	# p.resetJointStateMultiDof(urdf_body_id, joint_index, quat_final)
 
 # call this function AFTER applying the BT- and BP-joint angles for the urdf (using the function above) 
-def applyMMMPoseToURDFBody(urdf_body_id, px, py, pz, rx, ry, rz):
+def applyMMMRotationAndZeroTranslationToURDFBody(urdf_body_id, rx, ry, rz):
+	# get the rotation from the (hypothetical) world to the pelvis
+	tpcom, rpcom, tplcom, rplcom, tpf, rotation_pelvis_frame, v, omega = p.getLinkState(
+		urdf_body_id, 1, True, True)
+
+	# get the rotation from the (hypothetical) world to the chest
+	t_com, r_com = p.getBasePositionAndOrientation(urdf_body_id)
+	#m, lfr, lI, lIt, lIr, rest, rfr, sfr, cd, cs = p.getDynamicsInfo(bodyUniqueId,-1)
+	#t_tmp, r_tmp = p.invertTransform([0,0,0], lIr)
+	#t_tmp, rotation_chest_frame = p.multiplyTransforms([0,0,0], r_com, 
+	#	[0,0,0], r_tmp)
+	rotation_chest_frame = r_com
+
 	# get the rotation from the pelvis to the chest
-	p.get
+	t_tmp, r_tmp = p.invertTransform([0,0,0], rotation_pelvis_frame)
+	t_tmp, r_pelvis_to_chest = p.multiplyTransforms([0,0,0], r_tmp, 
+		[0,0,0], rotation_chest_frame)
+
+	# pre-multiply with rotation from MMM to my axis convention
+	t_tmp, r_mmm_pelvis_to_chest = p.multiplyTransforms([0,0,0],
+		p.getQuaternionFromEuler([-math.pi/2, math.pi, 0]), 
+		[0,0,0], r_pelvis_to_chest)
+
+	# generate rotation from world to pelvis MMM frame
+	#r_world_to_mmm_pelvis = generateQuaternionFromMMMRxRyRz(rx, ry, rz)
+	# Alternative (Z-Y-X order of rotations, not like for joints):
+	r_world_to_mmm_pelvis = p.getQuaternionFromEuler([rx, ry, rz])
+	#r_world_to_mmm_pelvis = generateQuaternionFromMMMGlobalRxRyRz(rx, ry, rz)
+
+	# compose rotation from world to my chest frame
+	t_tmp, r_world_to_chest = p.multiplyTransforms([0,0,0], r_world_to_mmm_pelvis,
+		[0,0,0], r_mmm_pelvis_to_chest)
+
+	# apply it to the base together with a zero translation
+	p.resetBasePositionAndOrientation(urdf_body_id,
+		[0, 0, 0], r_world_to_chest)
+
+def applyMMMTranslationToURDFBody(urdf_body_id, tx, ty, tz):
+	# get the translation to the pelvis frame
+	#tpcom, rpcom, tplcom, rplcom, translation_to_pelvis_frame, rpf, v, omega = p.getLinkState(
+	#	urdf_body_id, 1, True, True)
+
+	# get the translation to the left leg frame
+	tllcom, rllcom, tlllcom, rlllcom, translation_to_left_leg_frame, rllf, vll, omegall = p.getLinkState(
+		urdf_body_id, 1, True, True)
+
+	# get the translation to the right leg frame
+	trlcom, rrlcom, trllcom, rrllcom, translation_to_right_leg_frame, rrlf, vrl, omegarl = p.getLinkState(
+		urdf_body_id, 1, True, True)
+
+	t_base_com, r_base_com = p.getBasePositionAndOrientation(urdf_body_id)
+
+	t_phb_center_to_base_com = [0,0,0]
+	for i in range(3):
+		t_phb_center_to_base_com[i] = t_base_com[i] - 0.5*(
+			translation_to_right_leg_frame[i] + translation_to_left_leg_frame[i])
+
+	t_base_com = [
+		tx + t_phb_center_to_base_com[0],
+		ty + t_phb_center_to_base_com[1],
+		tz + t_phb_center_to_base_com[2]]
+
+	p.resetBasePositionAndOrientation(urdf_body_id, t_base_com, r_base_com)
+
 
 # load the human-adult model
 physicsClient = p.connect(p.GUI)
-human_adult_ID = p.loadURDF("../data/human_adult_with_feet.urdf",
+#human_adult_ID = p.loadURDF("../data/human_adult_with_feet.urdf",
+human_adult_ID = p.loadURDF("../data/human_adult_shaped.urdf",
 	flags=p.URDF_MAINTAIN_LINK_ORDER)
 
 #the following file is from https://motion-database.humanoids.kit.edu/details/motions/37/
 tree = ET.parse('../data/WalkingStraightForwards01.xml')
 root = tree.getroot()
 
+p.resetDebugVisualizerCamera(cameraDistance=3, 
+	cameraYaw=-90, cameraPitch=-25, cameraTargetPosition=[-2,0,0])
 
 time.sleep(1.0)
 
 joint_position_trajectories = np.zeros([len(root[0][3]), 44])
 pose_trajectories = np.zeros([len(root[0][3]), 6])
 
+initial_position_value_string_list = re.split(' ', root[0][3][0][1].text)
+x0 = float(initial_position_value_string_list[0])/1000
+y0 = float(initial_position_value_string_list[1])/1000
+z0 = float(initial_position_value_string_list[2])/1000
+
 for t in range(len(root[0][3])):
 
-	p.resetBasePositionAndOrientation(human_adult_ID,
-		[0, 0, 0], p.getQuaternionFromEuler([0,0,0]))
+	#p.resetBasePositionAndOrientation(human_adult_ID,
+	#	[0, 0, 0], p.getQuaternionFromEuler([0,0,0]))
 
 	value_string_list = re.split(' ', root[0][3][t][3].text)
 
@@ -74,6 +170,7 @@ for t in range(len(root[0][3])):
 
 	position_value_string_list = re.split(' ', root[0][3][t][1].text)
 	rotation_value_string_list = re.split(' ', root[0][3][t][2].text)
+
 	pose_trajectories[t, 0] = float(position_value_string_list[0])
 	pose_trajectories[t, 1] = float(position_value_string_list[1])
 	pose_trajectories[t, 2] = float(position_value_string_list[2])
@@ -171,6 +268,18 @@ for t in range(len(root[0][3])):
 	# left sole to left toes
 	p.resetJointState(human_adult_ID, 23, -float(value_string_list[26]))
 
+	# Base rotation and Zero Translation (for now)
+	applyMMMRotationAndZeroTranslationToURDFBody(human_adult_ID,
+		float(rotation_value_string_list[0]),
+		float(rotation_value_string_list[1]),
+		float(rotation_value_string_list[2]))
+
+	# Base translation
+	applyMMMTranslationToURDFBody(human_adult_ID,
+		float(position_value_string_list[0])/1000 - x0,
+		float(position_value_string_list[1])/1000 - y0,
+		float(position_value_string_list[2])/1000 - z0)
+
 	#p.stepSimulation()
 	time.sleep(0.01)
 
@@ -182,8 +291,6 @@ for j in range(44):
 	plt.plot(joint_position_trajectories[:, j])
 
 plt.show()
-
-quit()
 
 plt.plot(pose_trajectories[:, 0], 'r')
 plt.plot(pose_trajectories[:, 1], 'g')
--- script/show_urdf.py
+++ script/show_urdf.py
@@ -4,8 +4,58 @@ import pybullet as p
 import time
 import math
 
+def generateQuaternionFromMMMRxRyRz(rx, ry, rz):
+	q_intermediate = p.getQuaternionFromEuler([math.pi/2, 0, math.pi/2])
+	t, q_intermediate_inv = p.invertTransform([0,0,0], q_intermediate)
+	q_tmp = p.getQuaternionFromEuler([ry, rz, rx])
+	t, q_tmp = p.multiplyTransforms([0,0,0], q_intermediate, [0,0,0], q_tmp)
+	t, q = p.multiplyTransforms([0,0,0], q_tmp, [0,0,0], q_intermediate_inv)
+	return q
+
+# def generateQuaternionFromMMMGlobalRxRyRz(rx, ry, rz):
+# 	q_intermediate = p.getQuaternionFromEuler([0, -math.pi/2, math.pi])
+# 	t, q_intermediate_inv = p.invertTransform([0,0,0], q_intermediate)
+# 	q_tmp = p.getQuaternionFromEuler([rz, -ry, rx])
+# 	t, q_tmp = p.multiplyTransforms([0,0,0], q_intermediate, [0,0,0], q_tmp)
+# 	t, q = p.multiplyTransforms([0,0,0], q_tmp, [0,0,0], q_intermediate_inv)
+# 	return q
+
+def applyMMMRotationToURDFJoint(urdf_body_id, joint_index, rx, ry, rz, inverse=False):
+	q = generateQuaternionFromMMMRxRyRz(rx, ry, rz)
+	quat_tf_urdf = p.getQuaternionFromEuler([-math.pi/2, math.pi, 0])
+	translation, quat_tf_urdf_inv = p.invertTransform([0,0,0], quat_tf_urdf)
+	t, q = p.multiplyTransforms([0,0,0], quat_tf_urdf, [0,0,0], q)
+	t, q = p.multiplyTransforms([0,0,0], q, [0,0,0], quat_tf_urdf_inv)
+
+	if inverse:
+		t, q = p.invertTransform([0,0,0], q)
+
+	p.resetJointStateMultiDof(urdf_body_id, joint_index, q)
+
 physicsClient = p.connect(p.GUI)
-human_adult_ID = p.loadURDF("../data/human_adult.urdf", flags=p.URDF_MAINTAIN_LINK_ORDER)
+human_adult_ID = p.loadURDF("../data/human_adult_shaped.urdf",
+	flags=p.URDF_MAINTAIN_LINK_ORDER)
+
+p.resetDebugVisualizerCamera(cameraDistance=3, 
+	cameraYaw=-0, cameraPitch=-88, cameraTargetPosition=[-0,0,0])
+
+p.changeVisualShape(human_adult_ID, -1,
+	rgbaColor=[0, 1, 1, 0.5])
+
+applyMMMRotationToURDFJoint(human_adult_ID,
+	12, 0.1, -0.27, -0.2)
+applyMMMRotationToURDFJoint(human_adult_ID,
+	13, 0.1, 0.27, 0.2)
+# right arm to right forearm
+p.resetJointState(human_adult_ID, 14, -0.31)
+# left arm to left forearm
+p.resetJointState(human_adult_ID, 15, -0.4)
+
+
+time.sleep(100)
+p.disconnect()
+quit()
+
 
 #p.setGravity(0.0,0.0,-10.0)
 
