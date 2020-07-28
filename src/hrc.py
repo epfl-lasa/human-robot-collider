68f7b068503ad9f67a94b3839b9d98ef3c91cf49
Vaibhav Gupta <guptavaibhav0@gmail.com>
2020-07-28 13:38:41 +0200

    + Changed Walker package to Human package

--- src/hrc.py
+++ src/hrc.py
@@ -3,9 +3,9 @@ import logging
 
 import numpy as np
 
-from walker import Man, Child
-from qolo import Qolo
-from controller import NoControl, AdmittanceController
+from human import Man, Child
+from robot import Qolo
+from controller import NoControl, AdmittanceController, PassiveDSController
 from simulator import Simulator
 
 
@@ -19,6 +19,7 @@ robot_class = {
 controller_class = {
     "no_control": NoControl,
     "admittance": AdmittanceController,
+    "passive_ds": PassiveDSController,
 }
 
 
@@ -28,15 +29,15 @@ def parse_arguments():
         description="""Simulation of robot and human collision"""
     )
     parser.add_argument("-b", "--human",
-                        choices=["man", "child"],
+                        choices=[key for key in human_class],
                         default="man",
                         help="Human to collide with the robot (default = man)")
     parser.add_argument("-r", "--robot",
-                        choices=["qolo"],
+                        choices=[key for key in robot_class],
                         default="qolo",
                         help="Robot to collide with the human (default = qolo)")
     parser.add_argument("-c", "--controller",
-                        choices=["no_control", "admittance"],
+                        choices=[key for key in controller_class],
                         default="no_control",
                         help="Adaptive controller to use (default = no_control)")
     parser.add_argument("-g", "--gui",
@@ -52,10 +53,14 @@ if __name__ == '__main__':
     simulator = Simulator(
         Robot=robot_class[args.robot],
         Human=human_class[args.human],
-        Controller=controller_class[args.controller]
+        Controller=controller_class[args.controller],
+        show_GUI=args.gui,
     )
     result = [
         simulator.simulate(
+            robot_angle=robot_angle,
+            human_angle=human_angle,
+            gait_phase=gait_phase,
             robot_speed_factor=robot_speed_factor,
             human_speed_factor=human_speed_factor,
         )
